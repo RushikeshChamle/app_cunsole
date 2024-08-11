82a9ba31f2bc8ee10eec14641c686a6ba8dd17af
Rushikesh Chamle <rushikeshchamle23@gmail.com>
2024-08-11 18:32:00 +0530

    feat: project setup

---
+++ app_cunsole/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from app_cunsole.users.models import User
+from app_cunsole.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def _media_storage(settings, tmpdir) -> None:
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture()
+def user(db) -> User:
+    return UserFactory()
