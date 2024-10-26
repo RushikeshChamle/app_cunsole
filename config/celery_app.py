c7b7ae5755f5452c0de81440048f31d31ea1c506
Rushikesh Chamle <rushikeshchamle23@gmail.com>
2024-10-27 03:01:01 +0530

    updated the sign in 401 error api

--- config/celery_app.py
+++ config/celery_app.py
@@ -19,8 +19,11 @@
 import os
 from celery import Celery
 
+
+
+
 # Determine the correct settings module based on the environment variable
-env = os.environ.get("DJANGO_ENV", "production")  # Default to 'local' if not set
+env = os.environ.get("DJANGO_ENV", "local")  # Default to 'local' if not set
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", f"config.settings.{env}")
 
 # Create a new Celery app instance
@@ -28,6 +31,9 @@ app = Celery("app_cunsole")
 
 # Load task modules from all registered Django app configs
 app.config_from_object("django.conf:settings", namespace="CELERY")
+
+
+
 
 # Auto-discover tasks in all registered apps
 app.autodiscover_tasks()
