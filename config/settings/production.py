ae5fe07b7f6eb2a22ac307f3076c334e5d07c286
Rushikesh Chamle <rushikeshchamle23@gmail.com>
2025-07-14 23:32:20 +0530

    bugfix

--- config/settings/production.py
+++ config/settings/production.py
@@ -22,7 +22,7 @@
 
 
 
-# # DATABASES Data
+# # DATABASES Data store
 # # ------------------------------------------------------------------------------
 # DATABASES["default"]["CONN_MAX_AGE"] = env.int("CONN_MAX_AGE", default=60)
 
